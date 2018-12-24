c86fcc6e58a985a7843e36c1a70f5f335d610e5c
Yong Li Loh <YongLi.Loh@thehutgroup.com>
2018-12-24 15:16:54 +0000

    Delete all the unused code

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -25,55 +25,6 @@ class GildedRose {
                 backstageUpdate(items.get(i));
                 return;
             }
-
-            if (!items.get(i).name.equals("Aged Brie")
-                    && !items.get(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items.get(i).quality > 0) {
-                    if (!items.get(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items.get(i).quality = items.get(i).quality - 1;
-                    }
-                }
-            } else {
-                if (items.get(i).quality < 50) {
-                    items.get(i).quality = items.get(i).quality + 1;
-
-                    if (items.get(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items.get(i).sellIn < 11) {
-                            if (items.get(i).quality < 50) {
-                                items.get(i).quality = items.get(i).quality + 1;
-                            }
-                        }
-
-                        if (items.get(i).sellIn < 6) {
-                            if (items.get(i).quality < 50) {
-                                items.get(i).quality = items.get(i).quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items.get(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-                items.get(i).sellIn = items.get(i).sellIn - 1;
-            }
-
-            if (items.get(i).sellIn < 0) {
-                if (!items.get(i).name.equals("Aged Brie")) {
-                    if (!items.get(i).name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items.get(i).quality > 0) {
-                            if (!items.get(i).name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items.get(i).quality = items.get(i).quality - 1;
-                            }
-                        }
-                    } else {
-                        items.get(i).quality = items.get(i).quality - items.get(i).quality;
-                    }
-                } else {
-                    if (items.get(i).quality < 50) {
-                        items.get(i).quality = items.get(i).quality + 1;
-                    }
-                }
-            }
         }
     }
 
